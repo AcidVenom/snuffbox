f3c7157c492948a1217ac8b39be44d4edff28489
AcidVenom <info@danielkonings.com>
2014-04-16 23:42:17 +0200

    Doxygen stuff

--- snuffbox-doxygen/html/search/all_5.js
+++ snuffbox-doxygen/html/search/all_5.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['js_5fname',['JS_NAME',['../classsnuffbox_1_1_game.html#a19d726a070d51db1a22d3db2bed3a033',1,'snuffbox::Game']]],
   ['jsfunctionregister',['JSFunctionRegister',['../structsnuffbox_1_1_j_s_function_register.html',1,'snuffbox']]],
   ['jsobject',['JSObject',['../classsnuffbox_1_1_j_s_object.html',1,'snuffbox']]],
   ['jsregister',['JSRegister',['../classsnuffbox_1_1_j_s_register.html',1,'snuffbox']]],
