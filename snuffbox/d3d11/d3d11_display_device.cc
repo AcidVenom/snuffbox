fe53700ba4eceb5d4eafdcf41ed465b803b9cfcf
AcidVenom <info@danielkonings.com>
2014-10-28 09:15:59 +0100

    Flipping Y introduces a lot of problems

--- snuffbox/d3d11/d3d11_display_device.cc
+++ snuffbox/d3d11/d3d11_display_device.cc
@@ -1017,7 +1017,6 @@ namespace snuffbox
 		if (camera->type() == Camera::CameraType::kOrthographic)
 		{
 			projection_matrix_ = XMMatrixOrthographicRH(w*(1/camera->zoom()), h*(1/camera->zoom()), 1.0f, 1000.0f);
-			projection_matrix_ *= XMMatrixScaling(1, -1, 1);
 		}
 		else
 		{
